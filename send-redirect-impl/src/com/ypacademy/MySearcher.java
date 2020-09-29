d90d62cd53eabb3ada54a6dfd6b494aa4e89873c
Swapnil Warekar <swapnilwarekar@gmail.com>
2020-09-29 19:06:44 +0530

    Send redirect example

---
+++ send-redirect-impl/src/com/ypacademy/MySearcher.java
@@ -0,0 +1,18 @@
+package com.ypacademy;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MySearcher extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3994900546519227722L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("name");
+		response.sendRedirect("https://www.google.co.in/#q="+name);
+	}
+}
